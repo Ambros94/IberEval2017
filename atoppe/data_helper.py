ab1dd953c13f6727342ea89be2181911a4c20b01
Ambros94 <luca_ambro94@hotmail.it>
2017-03-22 12:49:34 +0100

    Added spacy spanish tokenizer

--- atoppe/data_helper.py
+++ atoppe/data_helper.py
@@ -1,9 +1,19 @@
+# encoding=utf8
 import csv
 import os
+import sys
+
+from spacy.es import Spanish
+
+# Fix encoding problems
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 script_dir = os.path.dirname(__file__)
 abs_train_path = os.path.join(script_dir, '../resources/coset-train.csv')
 abs_dev_path = os.path.join(script_dir, '../resources/coset-dev.csv')
+
+nlp = Spanish(path=None)
 
 
 def load_data():
@@ -26,6 +36,15 @@ def load_data():
 
     return (X_train[1:], y_train[1:]), (X_test, y_test)
 
-# (X_train, y_train), (X_test, y_test) = load_data()
-# print(X_train[0], y_train[0])
-# print len(X_train)
+
+def data_statistics(sentences):
+    for s in sentences:
+        tokens = nlp(unicode(s))
+        print("************")
+        print("Full sentence", unicode(s))
+        for t in tokens:
+            print(t)
+
+
+(X_train, y_train), (X_test, y_test) = load_data()
+data_statistics(X_train)
