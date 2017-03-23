37b18d55a569667b8b58985144f9c2de90b0c5eb
Ambros94 <luca_ambro94@hotmail.it>
2017-03-23 13:13:15 +0100

    Changed code structure to class

--- atoppe/data_helper.py
+++ atoppe/data_helper.py
@@ -16,40 +16,54 @@ abs_dev_path = os.path.join(script_dir, '../resources/coset-dev.csv')
 nlp = Spanish(path=None)
 
 
-def load_data():
-    """
-    Loads data form file, the train set contains also the dev
-    :return: (X_train, y_train), (X_test, y_test)
-    """
-    X_train, y_train, X_test, y_test = [], [], [], []
-    # Train set
-    with open(abs_train_path, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in spamreader:
-            X_train.append(row[1])
-            y_train.append(row[2])
-    # Dev set
-    with open(abs_dev_path, 'rb') as csvfile
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in spamreader:
-            X_train.append(row[1])
-            y_train.append(row[2])
+class DataLoader:
+    def __init__(self):
+        self.X_train = []
+        self.y_train = []
+        self.X_test = []
+        self.y_test = []
 
-    # TODO Go on with data pre-processing, at least join hash tags in a single word
-    # p.s Hashtags are not in the dictionary anymore, we should come up with something
-    #  #perro is not in the dict, perro is
+    def load_data(self, tokenize=True, join_hash_tags=False):
+        """
+        Loads data form file, the train set contains also the dev
+        :return: (X_train, y_train), (X_test, y_test)
+        """
 
-    return (X_train[1:], y_train[1:]), (X_test, y_test)
+        # Train set
+        with open(abs_train_path, 'rb') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                self.X_train.append(row[1])
+                self.y_train.append(row[2])
+        # Dev set
+        with open(abs_dev_path, 'rb') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                self.X_train.append(row[1])
+                self.y_train.append(row[2])
+        if tokenize:
+            self.X_train = DataLoader.tokenize(self.X_train)
+            self.X_test = DataLoader.tokenize(self.X_test)
+        if join_hash_tags:
+            self.join_hash_tags()
+
+        return (self.X_train, self.y_train), (self.X_test, self.y_test)
+
+    @classmethod
+    def tokenize(cls, sentences_array):
+        """
+        Tokenize the sentence array given as input
+        :param sentences_array: Iterable of sentences that will be tokenize
+        :return: Array of arrays containing tokenized sentences
+        """
+        tokenize_array = []
+        for s in sentences_array:
+            tokenize_array.append([t for t in nlp(unicode(s))])
+        return tokenize_array
+
+    def join_hash_tags(self):
+        pass
 
 
-def data_statistics(sentences):
-    for s in sentences:
-        tokens = nlp(unicode(s))
-        print("************")
-        print("Full sentence", unicode(s))
-        for t in tokens:
-            print(t)
-
-
-(X_train, y_train), (X_test, y_test) = load_data()
-data_statistics(X_train)
+(X_train, y_train), (X_test, y_test) = DataLoader().load_data(tokenize=True)
+print(X_train[0], y_train[0])
