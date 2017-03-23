0fded9af5075f7ead61e6a901609a71e74d04707
Ambros94 <luca_ambro94@hotmail.it>
2017-03-23 16:55:23 +0100

    Added word2vec

--- atoppe/data_helper.py
+++ atoppe/data_helper.py
@@ -3,6 +3,7 @@ import csv
 import os
 import sys
 
+from gensim.models import KeyedVectors
 from spacy.es import Spanish
 
 # Fix encoding problems
@@ -23,7 +24,7 @@ class DataLoader:
         self.X_test = []
         self.y_test = []
 
-    def load_data(self, tokenize=True, join_hash_tags=False):
+    def load_data(self, tokenize=True, join_hash_tags=False, word2vec=True):
         """
         Loads data form file, the train set contains also the dev
         :return: (X_train, y_train), (X_test, y_test)
@@ -46,6 +47,9 @@ class DataLoader:
             self.X_test = DataLoader.tokenize(self.X_test)
         if join_hash_tags:
             self.join_hash_tags()
+        if word2vec:
+            self.X_train = DataLoader.word2vec(self.X_train)
+            self.X_test = DataLoader.word2vec(self.X_test)
 
         return (self.X_train, self.y_train), (self.X_test, self.y_test)
 
@@ -64,6 +68,35 @@ class DataLoader:
     def join_hash_tags(self):
         pass
 
+    @classmethod
+    def word2vec(cls, tokenized_senteces_array):
+        # Creating the model
+        print("Started vector loading!")
+        es_model = KeyedVectors.load_word2vec_format('../resources/wiki.es.vec')
+        print("Successfully loaded all vectors!")
+        match = 0
+        miss = 0
+        try:
+            print es_model.word_vec("perro")
+        except KeyError:
+            print "Not in vocab str"
+        try:
+            print es_model.word_vec(u"perro")
+        except KeyError:
+            print "Not in vocab unicode"
+        for s in tokenized_senteces_array:
+            for t in s:
+                try:
+                    print t
+                    print es_model.word_vec(str(t))
+                    print "Found"
+                    match += 1
+                except KeyError:
+                    print "Not in vocab"
+                    miss += 1
+        print("{} Words does NOT have any vector".format(miss))
+        print("{} Words DOES have any vector".format(match))
 
-(X_train, y_train), (X_test, y_test) = DataLoader().load_data(tokenize=True)
+
+(X_train, y_train), (X_test, y_test) = DataLoader().load_data(tokenize=True, word2vec=True)
 print(X_train[0], y_train[0])
