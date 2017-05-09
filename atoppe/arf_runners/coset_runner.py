39f96138828b77ad1b77309dad887e1d643ba244
Ambros94 <luca.ambrosini@supsi.ch>
2017-05-09 14:02:43 +0200

    Updating results

--- atoppe/arf_runners/coset_runner.py
+++ atoppe/arf_runners/coset_runner.py
@@ -23,7 +23,7 @@ b_lstm.run(metrics=[metrics.fbeta_score], max_len=max_len,
            batch_size=32, embedding_dims=128, recurrent_units=64, dropout=0.1, language=language,
            epochs=2)
 b_lstm_f1_macro = b_lstm.test_f1_macro()
-b_lstm.persist_result(filename="b_lstm.atoppe.result.csv")
+# b_lstm.persist_result(filename="b_lstm.atoppe.result.csv")
 
 cnn = CNNModel(data_function=data_function, decode_function=coset.decode_label, persist_function=coset.persist_solution,
                test_function=coset.load_test)
@@ -34,7 +34,7 @@ cnn.run(metrics=[metrics.fbeta_score], maxlen=max_len,
         dropout_final=0.2, dilation_rate=1, padding='same',
         hidden_dims=50, epochs=3, language=language)
 cnn_f1_macro = cnn.test_f1_macro()
-cnn.persist_result(filename="cnn.atoppe.result.csv")
+# cnn.persist_result(filename="cnn.atoppe.result.csv")
 
 lstm = LSTMModel(data_function=data_function, decode_function=coset.decode_label,
                  persist_function=coset.persist_solution,
@@ -43,7 +43,7 @@ lstm.run(metrics=[metrics.fbeta_score], maxlen=max_len, embedding_dims=100, lang
          batch_size=32,
          dropout=0.2, recurrent_dropout=0.4, lstm_units=128, epochs=4)
 lstm_f1_macro = lstm.test_f1_macro()
-lstm.persist_result(filename="lstm.atoppe.result.csv")
+# lstm.persist_result(filename="lstm.atoppe.result.csv")
 
 # cnn_lstm = CnnLstmModel(data_function=data_function, decode_function=coset.decode_label,
 #                        persist_function=coset.persist_solution,
@@ -60,7 +60,7 @@ fast_text.run(metrics=[metrics.fbeta_score], maxlen=max_len,
               ngram_range=2, embedding_dims=300, hidden_dims=100, language=language,
               batch_size=32, epochs=6)
 fast_text_f1_macro = fast_text.test_f1_macro()
-fast_text.persist_result(filename="fast_text.atoppe.result.csv")
+# fast_text.persist_result(filename="fast_text.atoppe.result.csv")
 
 kim = KimModel(data_function=data_function, decode_function=coset.decode_label, persist_function=coset.persist_solution,
                test_function=coset.load_test)
@@ -69,7 +69,7 @@ kim.run(metrics=[metrics.fbeta_score], maxlen=max_len,
         dropout=0.5, dropout_final=0.5, trainable=True,
         recurrent_units=128, epochs=3, padding='same', dilation_rate=3, pool_size=5)
 kim_f1_macro = kim.test_f1_macro()
-kim.persist_result(filename="kim.atoppe.result.csv")
+# kim.persist_result(filename="kim.atoppe.result.csv")
 
 with open("../coset-" + strftime("%Y%m%d_%H%M%S", gmtime()) + ".log", 'w') as outcsv:
     writer = csv.writer(outcsv, delimiter='\t')
--- atoppe/nlp_utils/tweets_preprocessor.py
+++ atoppe/nlp_utils/tweets_preprocessor.py
@@ -1,20 +1,19 @@
 import preprocessor as p
 from nltk import TweetTokenizer
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SpanishStemmer
 
 
 def _clean_tweet(tweet):
-    p.set_options(p.OPT.URL, p.OPT.RESERVED)
+    p.set_options(p.OPT.URL, p.OPT.RESERVED, p.OPT.NUMBER)
     tweet = p.clean(tweet)
-    p.set_options(p.OPT.EMOJI, p.OPT.SMILEY, p.OPT.NUMBER)
+    p.set_options(p.OPT.MENTION, p.OPT.SMILEY, p.OPT.EMOJI)
     tweet = p.tokenize(tweet)
     tokenizer = TweetTokenizer(reduce_len=True)
     word_list = tokenizer.tokenize(tweet)
     filtered_words = [word for word in word_list if word not in stopwords.words('spanish')]
-    stemmer = SpanishStemmer()
-    stemmed_words = [stemmer.stem(word=word) for word in filtered_words]
-    tweet = " ".join([word for word in stemmed_words])
+    # stemmer = SpanishStemmer()
+    # stemmed_words = [stemmer.stem(word=word) for word in filtered_words]
+    tweet = " ".join([word for word in filtered_words])
     return tweet
 
 
