01e9b58e246f1144edd1be04cc41f5c9dbb85770
Ambros94 <luca.ambrosini@supsi.ch>
2017-05-21 18:46:33 +0200

    Starting data gathering for pre-processing on stance ca

--- atoppe/arf_runners/optimizer.py
+++ atoppe/arf_runners/optimizer.py
@@ -1,30 +1,48 @@
-from arf_runners.gender_runner_ca import run
+from arf_runners.stance_runner_ca import run
 from nlp_utils.tweets_preprocessor import *
 
-run(st_sw_cl_mt_num)
-run(st_sw_cl_mt_num)
-run(st_sw_cl_mt_num)
+
 
 run(st_sw_cl_mt_num_em)
-run(st_sw_cl_mt_num_em)
-run(st_sw_cl_mt_num_em)
+
 
 run(st_sw_cl_mt_num_em_ht)
-run(st_sw_cl_mt_num_em_ht)
-run(st_sw_cl_mt_num_em_ht)
+
 
 run(sw_cl_mt_num_em_ht)
-run(sw_cl_mt_num_em_ht)
-run(sw_cl_mt_num_em_ht)
+
 
 run(cl)
-run(cl)
-run(cl)
+
 
 run(cl_em)
-run(cl_em)
-run(cl_em)
+
+
+run(cl_mt_num_em)
+
+run(st)
+run(st)
+
+run(nothing)
+run(nothing)
 
 run(cl_mt_num_em)
 run(cl_mt_num_em)
-run(cl_mt_num_em)
+run(cl_em)
+run(cl_em)
+run(cl)
+run(cl)
+run(sw_cl_mt_num_em_ht)
+run(sw_cl_mt_num_em_ht)
+run(st_sw_cl_mt_num_em_ht)
+run(st_sw_cl_mt_num_em_ht)
+run(st_sw_cl_mt_num_em)
+run(st_sw_cl_mt_num_em)
+run(st_sw_cl_mt_num)
+run(st_sw_cl_mt_num)
+run(st_sw_cl_mt)
+run(st_sw_cl_mt)
+run(st_sw_cl)
+run(st_sw_cl)
+run(st_sw)
+run(st_sw)
